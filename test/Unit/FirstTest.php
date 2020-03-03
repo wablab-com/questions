5f3837337e46d75fe61b2cb66cc9571c69fa02b1
Ibrahim I Musabah <i.musabah92@gmail.com>
2020-03-03 13:00:40 +0200

    =phpunit 1

---
+++ test/Unit/FirstTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_basic_test()
+    {
+        $this->assertTrue(true);
+    }
+
+
+}
