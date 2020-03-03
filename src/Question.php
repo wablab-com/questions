ee2c7d6ed8f6ef73ace436fbb4f30655055fbc1e
Ibrahim I Musabah <i.musabah92@gmail.com>
2020-03-03 17:11:25 +0200

    =save

--- src/Question.php
+++ src/Question.php
@@ -2,104 +2,27 @@
 
 
 namespace WabLab\Questions;
+use \WabLab\Questions\Contracts\Question as QuestionInterface;
 
-
-class Question
+class Question implements  QuestionInterface
 {
-    /**
-     * @var string
-     */
-    protected $text;
 
     /**
      * @var string
      */
-    protected $imagePath;
-
-    /**
-     * @var string
-     */
-    protected $type;
-
-    /**
-     * @var array
-     */
-    protected $properties;
-
-    /**
-     * @return string
-     */
-
     protected $title;
     /**
      * @var string
      */
     protected $description;
-
-    public function getText(): string
-    {
-        return $this->text;
-    }
-
-    /**
-     * @param string $text
-     */
-    public function setText(string $text): void
-    {
-        $this->text = $text;
-    }
-
-    /**
-     * @return string
-     */
-    public function getImagePath(): string
-    {
-        return $this->imagePath;
-    }
-
-    /**
-     * @param string $imagePath
-     */
-    public function setImagePath(string $imagePath): void
-    {
-        $this->imagePath = $imagePath;
-    }
-
-    /**
-     * @return string
-     */
-    public function getType(): string
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param string $type
-     */
-    public function setType(string $type): void
-    {
-        $this->type = $type;
-    }
-
-    /**
-     * @return array
-     */
-    public function getProperties(): array
-    {
-        return $this->properties;
-    }
-
-    /**
-     * @param array $properties
-     */
-    public function setProperties(array $properties): void
-    {
-        $this->properties = $properties;
-    }
-
     /**
      * @param string $title
      */
+    /**
+     * @var int
+     */
+    protected $points;
+
     public function setTitle(string $title):void
     {
         $this->title = $title;
@@ -113,20 +36,33 @@ class Question
         return $this->title;
     }
 
+    public function setPoints(int $points)
+    {
+        $this->points = $points;
+    }
+
     /**
-     * @return mixed
+     * @return int
      */
-    public function getDescription()
+    public function getPoints(): int
+    {
+        return $this->points;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
     {
         return $this->description;
+
     }
 
     /**
-     * @param string $description
+     * @param string $title
      */
-    public function setDescription(string $description): void
+    public function setDescription(string $title)
     {
-        $this->description = $description;
+        $this->description = $title;
     }
-
 }
