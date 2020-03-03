8e85fb75e3e8e99d0170d0809c5904ad90397b22
Ibrahim I Musabah <i.musabah92@gmail.com>
2020-03-03 15:50:32 +0200

    =save

--- src/MultipleChoiceQuestion.php
+++ src/MultipleChoiceQuestion.php
@@ -4,7 +4,26 @@
 namespace WabLab\Questions;
 use \WabLab\Questions\Contracts\MultipleChoiceQuestion as MultipleChoiceQuestions;
 
-abstract class MultipleChoiceQuestion extends Question implements MultipleChoiceQuestions
+ class MultipleChoiceQuestion extends Question implements MultipleChoiceQuestions
 {
+     /**
+      * @var int
+      */
+    protected $points;
 
-}
+     /**
+      * @return mixed
+      */
+     public function getPoints():int
+     {
+         return $this->points;
+     }
+
+     /**
+      * @param int $points
+      */
+     public function setPoints(int $points): void
+     {
+         $this->points = $points;
+     }
+ }
--- src/MultipleSelectionQuestion.php
+++ src/MultipleSelectionQuestion.php
@@ -5,6 +5,26 @@ namespace WabLab\Questions;
 use \WabLab\Questions\Contracts\MultipleChoiceQuestion as MultipleChoiceQuestions;
 
 
-abstract class MultipleSelectionQuestion extends Question implements MultipleChoiceQuestions
+ class MultipleSelectionQuestion extends Question implements MultipleChoiceQuestions
 {
+     /**
+      * @var int
+      */
+    protected $points;
+
+     /**
+      * @return mixed
+      */
+     public function getPoints():int
+     {
+         return $this->points;
+     }
+
+     /**
+      * @param int $points
+      */
+     public function setPoints(int $points): void
+     {
+         $this->points = $points;
+     }
  }
